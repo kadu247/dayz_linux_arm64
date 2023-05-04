34613cbaf53d859344bf700e29ec71f729500a66
ᴎoᴎame <pinto_3383@hotmail.com>
2023-05-04 10:29:14 +0100

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -8,64 +8,52 @@ SERVER_CONFIG="$SERVER_DIR/serverDZ.cfg"
 SERVER_PROFILE="$SERVER_DIR/profile"
 SERVER_PORT=2302
 SERVER_CPU_COUNT=4
+SERVER_FPS=60
 
-# Check if server files exists, create and install dayz server if it doesn't exist
+# Check if SERVER_DIR exists and create it if it doesn't exist
 if [ ! -d "$SERVER_DIR" ]; then
   echo "Creating server directory: $SERVER_DIR"
   mkdir -p "$SERVER_DIR"
-  $STEAMCMD_DIR/steamcmd_arm.sh +login anonymous +force_install_dir $SERVER_DIR +app_update $APPID +quit
+  $STEAMCMD_DIR/steamcmdmod.sh +login anonymous +force_install_dir $SERVER_DIR +app_update $APPID +quit
 fi
 
 cd "$SERVER_DIR"
 
-export LD_LIBRARY_PATH="$SERVER_DIR:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$SERVER_DIR:$STEAMCMD_DIR/linux32:$LD_LIBRARY_PATH"
 
 # Function to check if Server is running
 is_app_running() {
   pgrep -f "DayZServer" > /dev/null
 }
 
-# Function to get the latest version number from Steam
-get_latest_version() {
-  $STEAMCMD_DIR/steamcmd_arm.sh +login anonymous +app_info_print $APPID +quit | grep -m1 -Po '"buildid" "\K\d+'
-}
-
 # Function to validate the server
 validate_server() {
-  $STEAMCMD_DIR/steamcmd_arm.sh +login anonymous +force_install_dir $SERVER_DIR +app_update $APPID validate +quit
+  $STEAMCMD_DIR/steamcmdmod.sh +login anonymous +force_install_dir $SERVER_DIR +app_update $APPID validate +quit
 }
 
 # Function to update the server
 update_server() {
-  $STEAMCMD_DIR/steamcmd_arm.sh +login anonymous +force_install_dir $SERVER_DIR +app_update $APPID +quit
+  $STEAMCMD_DIR/steamcmdmod.sh +login anonymous +force_install_dir $SERVER_DIR +app_update $APPID +quit
 }
 
 # Function to start the server
 start_server() {
-  box64 "$SERVER_DIR/DayZServer" -config="$SERVER_CONFIG" -profiles="$SERVER_PROFILE" -port="$SERVER_PORT" -cpuCount="$SERVER_CPU_COUNT" -freezecheck
+  box64 "$SERVER_DIR/DayZServer" -config="$SERVER_CONFIG" -profiles="$SERVER_PROFILE" -port="$SERVER_PORT" -cpuCount="$SERVER_CPU_COUNT" -limitFPS="$SERVER_FPS" -adminLog -freezecheck
 }
 
 # Loop indefinitely
 while true; do
   # Check if Server is running
   if ! is_app_running; then
-    echo "Checking for updates..."
-    current_version=$(cat "$SERVER_DIR/steamapps/appmanifest_$APPID.acf" | grep -Po '"buildid" "\K\d+')
-    latest_version=$(get_latest_version)
-
-    if [ "$current_version" != "$latest_version" ]; then
-      echo "An update is available. Validating server files..."
-      validate_server
-      echo "Updating server..."
-      update_server
-    else
-      echo "Server is up to date."
-    fi
-
-    echo "Starting server..."
+    echo "$(date) Validating server files..."
+    validate_server
+    echo "$(date) Updating server..."
+    update_server
+    echo "$(date) Starting server..."
     start_server
   fi
 
-  # Sleep for 15 seconds
-  sleep 15
+  # Sleep for 30 seconds
+  sleep 30
+  echo "$(date) Server is running ..."
 done
