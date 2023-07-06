9a0bdb04c6e973910ad861fd1d7a1cbf9ee967c0
ᴎoᴎame <pinto_3383@hotmail.com>
2023-07-06 01:49:44 +0100

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Set variables
 APPID=1042420
 STEAMCMD_DIR="$HOME/steamcmd"
@@ -11,30 +13,33 @@ SERVER_CPU_COUNT=4
 SERVER_FPS=60
 SERVER_IP="0.0.0.0"
 
+CUSTOM_INIT="$HOME/scripts/init.c"
+USE_CUSTOM_INIT="1"
+
 # Check if SERVER_DIR exists and create it if it doesn't exist
 if [ ! -d "$SERVER_DIR" ]; then
   echo "Creating server directory: $SERVER_DIR"
   mkdir -p "$SERVER_DIR"
-  $STEAMCMD_DIR/steamcmdmod.sh +login anonymous +force_install_dir $SERVER_DIR +app_update $APPID +quit
+  "$STEAMCMD_DIR/steamcmdmod.sh" +login anonymous +force_install_dir "$SERVER_DIR" +app_update "$APPID" +quit
 fi
 
 cd "$SERVER_DIR"
 
-export LD_LIBRARY_PATH="$SERVER_DIR:$STEAMCMD_DIR/linux32:$LD_LIBRARY_PATH"
+export LD_LIBRARY_PATH="$SERVER_DIR:$STEAMCMD_DIR/linux32:$HOME/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH"
 
 # Function to check if Server is running
 is_app_running() {
   pgrep -f "DayZServer" > /dev/null
 }
 
-# Function to validate the server
-validate_server() {
-  $STEAMCMD_DIR/steamcmdmod.sh +login anonymous +force_install_dir $SERVER_DIR +app_update $APPID validate +quit
-}
-
 # Function to update the server
 update_server() {
-  $STEAMCMD_DIR/steamcmdmod.sh +login anonymous +force_install_dir $SERVER_DIR +app_update $APPID +quit
+  "$STEAMCMD_DIR/steamcmd_arm.sh" +login anonymous +force_install_dir "$SERVER_DIR" +app_update "$APPID" +quit
+}
+
+# Function to validate the server
+validate_server() {
+  "$STEAMCMD_DIR/steamcmd_arm.sh" +login anonymous +force_install_dir "$SERVER_DIR" +app_update "$APPID" validate +quit
 }
 
 # Function to start the server
@@ -46,15 +51,24 @@ start_server() {
 while true; do
   # Check if Server is running
   if ! is_app_running; then
-    echo "$(date) Validating server files..."
-    validate_server
     echo "$(date) Updating server..."
     update_server
+    echo "$(date) Validating server files..."
+    validate_server
+    if [[ "$USE_CUSTOM_INIT" == "1" ]]; then
+      echo "$(date) Use custom init.c..."
+      # Remove the existing init.c file
+      rm "$SERVER_DIR/mpmissions/dayzOffline.chernarusplus/init.c" || true
+      # Copy the custom init.c file to the server directory
+      cp "$CUSTOM_INIT" "$SERVER_DIR/mpmissions/dayzOffline.chernarusplus/init.c"
+    elif [[ "$USE_CUSTOM_INIT" == "0" ]]; then
+      echo "$(date) Custom init.c not used."
+    fi
     echo "$(date) Starting server..."
     start_server
   fi
 
-  # Sleep for 30 seconds
-  sleep 30
+  # Sleep for 60 seconds
+  sleep 60
   echo "$(date) Server is running ..."
 done
